3ff1553543e75753c9f5a330da2456332beb5416
Abraham <eshkim1@gmail.com>
2026-05-12 15:41:41 +0200

    test: add dom and tests

---
+++ tests/dom.test.js
@@ -0,0 +1,22 @@
+import { describe, it, expect } from "vitest";
+import { Window } from "happy-dom";
+import { createCountryElement } from "../src/dom.js";
+
+const window = new Window();
+global.document = window.document;
+
+describe("DOM tests", () => {
+  it("should create a country element", () => {
+    const country = {
+      name: {
+        common: "Sweden",
+      },
+    };
+
+    const element = createCountryElement(country);
+
+    expect(element.nodeName).toBe("DIV");
+    expect(element.className).toBe("country");
+    expect(element.textContent).toBe("SWEDEN");
+  });
+});
