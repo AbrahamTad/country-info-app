720ab223574a18d448c95cc146ee0c839e1edc06
Abraham <eshkim1@gmail.com>
2026-05-13 10:37:31 +0200

    test: add utility function tests

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,25 +1,32 @@
+// Import Vitest functions
 import { describe, it, expect } from "vitest";
+
+// Import functions to test
 import {
   formatCountryName,
   countCountries,
   filterCountries,
 } from "../src/utils.js";
 
+// Group utility tests
 describe("utils functions", () => {
+  // Test uppercase function
   it("should format country name to uppercase", () => {
     expect(formatCountryName("Sweden")).toBe("SWEDEN");
   });
 
+  // Test count function
   it("should count countries", () => {
     const countries = ["Sweden", "Norway", "Denmark"];
+
     expect(countCountries(countries)).toBe(3);
   });
 
+  // Test filter function
   it("should filter countries by search text", () => {
     const countries = [
       { name: { common: "Sweden" } },
       { name: { common: "Norway" } },
-      { name: { common: "Denmark" } },
     ];
 
     const result = filterCountries(countries, "swe");
