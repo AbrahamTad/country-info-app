097514ea990e75e914bcaf541b50a99e12e49c0a
Abraham <eshkim1@gmail.com>
2026-05-13 10:41:56 +0200

    test:  cypress e2e tests

--- cypress/e2e/country.cy.js
+++ cypress/e2e/country.cy.js
@@ -1,19 +1,31 @@
+// Group E2E tests
 describe("World Explorer App", () => {
+  // Test loading countries
   it("should load many countries when user clicks button", () => {
     cy.visit("http://localhost:5173");
 
     cy.get("#loadCountriesBtn").click();
 
     cy.get(".country").should("have.length", 12);
-    cy.get(".country").first().should("contain.text", "Capital");
   });
 
+  // Test search function
   it("should search country by name", () => {
     cy.visit("http://localhost:5173");
 
     cy.get("#searchInput").type("Sweden");
+
     cy.get("#loadCountriesBtn").click();
 
     cy.get(".country").should("contain.text", "SWEDEN");
+  });
+
+  // Test intro text
+  it("should show country intro", () => {
+    cy.visit("http://localhost:5173");
+
+    cy.get("#loadCountriesBtn").click();
+
+    cy.get(".country").first().should("contain.text", "located in");
   });
 });
