5562a3d6720cef420340e13a71056770b20ea1a6
Abraham <eshkim1@gmail.com>
2026-05-13 10:39:50 +0200

    test: updat dom rendering tests

--- tests/dom.test.js
+++ tests/dom.test.js
@@ -1,24 +1,30 @@
+// Import Vitest functions
 import { describe, it, expect } from "vitest";
+
+// Import happy-dom
 import { Window } from "happy-dom";
+
+// Import DOM functions
 import { createCountryElement } from "../src/dom.js";
 
+// Create fake browser window
 const window = new Window();
+
 global.document = window.document;
 
+// Group DOM tests
 describe("DOM tests", () => {
-  it("should create a country element with name and capital", () => {
+  // Test country element creation
+  it("should create country element", () => {
     const country = {
-      name: {
-        common: "Sweden",
-      },
+      name: { common: "Sweden" },
       capital: ["Stockholm"],
     };
 
     const element = createCountryElement(country);
 
-    expect(element.nodeName).toBe("DIV");
     expect(element.className).toBe("country");
+
     expect(element.textContent).toContain("SWEDEN");
-    expect(element.textContent).toContain("Capital: Stockholm");
   });
 });
