528b0e5e9df3b2ce0731d2299c22edc5c05f78d7
Abraham <eshkim1@gmail.com>
2026-05-13 10:00:36 +0200

    feat: fetch more country details from api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 export async function fetchCountries() {
   const response = await fetch(
-    "https://restcountries.com/v3.1/all?fields=name,capital",
+    "https://restcountries.com/v3.1/all?fields=name,capital,region,subregion,population,languages,flags",
   );
 
   if (!response.ok) {
