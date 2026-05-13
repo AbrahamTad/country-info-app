7200615a684e042045d4409fe3625cb7d80b3450
Abraham <eshkim1@gmail.com>
2026-05-13 09:26:51 +0200

    test: add search utility test

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,5 +1,9 @@
 import { describe, it, expect } from "vitest";
-import { formatCountryName, countCountries } from "../src/utils.js";
+import {
+  formatCountryName,
+  countCountries,
+  filterCountries,
+} from "../src/utils.js";
 
 describe("utils functions", () => {
   it("should format country name to uppercase", () => {
@@ -8,7 +12,18 @@ describe("utils functions", () => {
 
   it("should count countries", () => {
     const countries = ["Sweden", "Norway", "Denmark"];
-
     expect(countCountries(countries)).toBe(3);
+  });
+
+  it("should filter countries by search text", () => {
+    const countries = [
+      { name: { common: "Sweden" } },
+      { name: { common: "Norway" } },
+      { name: { common: "Denmark" } },
+    ];
+
+    const result = filterCountries(countries, "swe");
+
+    expect(result[0].name.common).toBe("Sweden");
   });
 });
